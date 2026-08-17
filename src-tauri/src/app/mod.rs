1752275649bebc0e50d213d0ed7fa4c5bbb7ea0c
taran.ia <taran.ia@dns-shop.ru>
2026-08-17 19:47:48 +1000

    реализую слой приложения

--- src-tauri/src/app/mod.rs
+++ src-tauri/src/app/mod.rs
@@ -1,5 +1,5 @@
 use crate::domain::hotkey::{Action, Combo, DomainError, Shortcut};
-use crate::domain::repository::{HotkeyRegistry, Launcher, ShortcutRepository};
+use crate::domain::repository::{HotkeyRegistry, Launcher, RegistryError, ShortcutRepository};
 use std::sync::Arc;
 use thiserror::Error;
 use tracing::error;
@@ -16,7 +16,7 @@ pub enum CreateError {
 }
 
 #[derive(Debug, Error)]
-pub enum DeleteError {
+pub enum EditError {
     #[error("Комбинации не существует")]
     NotFound,
 
@@ -54,48 +54,111 @@ impl App {
             return Err(CreateError::AlreadyExist);
         }
 
-        if shortcut.enabled {
-            self.registry
-                .register(&shortcut.id, &shortcut.combo)
-                .map_err(|e| CreateError::Internal(e.to_string()))?;
-        }
+        let rollback = self
+            .register_shortcut(&shortcut)
+            .map_err(|e| CreateError::Internal(e.to_string()))?;
 
         if let Err(e) = self.repo.save(&shortcut) {
-            if shortcut.enabled {
-                if let Err(e) = self.registry.unregister(&shortcut.id) {
-                    error!("отмена регистрации: {e}")
-                }
+            if let Err(e) = rollback() {
+                error!("отмена регистрации: {e}")
             }
+
             return Err(CreateError::Internal(e.to_string()));
         }
 
         Ok(shortcut)
     }
-    pub fn delete_shortcut(&self, id: &str) -> Result<(), DeleteError> {
+    pub fn delete_shortcut(&self, id: &str) -> Result<(), EditError> {
         if self
             .repo
             .get(&id)
-            .map_err(|_| DeleteError::Internal(id.to_string()))?
+            .map_err(|_| EditError::Internal(id.to_string()))?
             .is_none()
         {
-            return Err(DeleteError::NotFound);
+            return Err(EditError::NotFound);
         }
 
         self.registry
             .unregister(id)
-            .map_err(|e| DeleteError::Internal(e.to_string()))?;
+            .map_err(|e| EditError::Internal(e.to_string()))?;
 
         self.repo
             .delete(&id)
-            .map_err(|e| DeleteError::Internal(e.to_string()))?;
+            .map_err(|e| EditError::Internal(e.to_string()))?;
 
         Ok(())
     }
 
-    pub fn set_enable_shortcut(&self) {}
-    pub fn list_shortcut(&self) -> Result<Vec<Shortcut>, ()> {
-        self.repo.all().map_err(|_| ()) // todo(доработать ошибку)
+    pub fn set_enable_shortcut(&self, id: &str, enabled: bool) -> Result<(), EditError> {
+        let mut shortcut = self
+            .repo
+            .get(&id)
+            .map_err(|e| EditError::Internal(e.to_string()))?
+            .ok_or(EditError::NotFound)?;
+
+        shortcut.enabled = enabled;
+
+        let rollback = self
+            .register_shortcut(&shortcut)
+            .map_err(|e| EditError::Internal(e.to_string()))?;
+
+        if let Err(e) = self.repo.save(&shortcut) {
+            if let Err(e) = rollback() {
+                error!("откат изменений: {e}")
+            };
+            return Err(EditError::Internal(e.to_string()));
+        };
+
+        Ok(())
     }
-    pub fn register_all_shortcut(&self) {}
+
+    fn register_shortcut(
+        &self,
+        shortcut: &Shortcut,
+    ) -> Result<fn() -> Result<(), RegistryError>, RegistryError> {
+        if shortcut.enabled {
+            self.registry.register(&shortcut.id, &shortcut.combo)?
+        } else {
+            self.registry.unregister(&shortcut.id)?
+        }
+
+        Ok(|| -> Result<(), RegistryError> {
+            if shortcut.enabled {
+                self.registry.unregister(&shortcut.id)
+            } else {
+                self.registry.register(&shortcut.id, &shortcut.combo)
+            }
+        })
+    }
+
+    pub fn list_shortcut(&self) -> Result<Vec<Shortcut>, String> {
+        self.repo.all().map_err(|e| e.to_string()) // todo(доработать ошибку)
+    }
+
+    pub fn register_all_shortcut(&self) -> Result<(), String> {
+        let shortcut_list = self.list_shortcut()?;
+        let mut rollback_list = Vec::new();
+
+        for shortcut in shortcut_list.iter() {
+            // регистрируем хоткей
+            match self.register_shortcut(shortcut) {
+                Ok(rollback) => {
+                    // если успешно зарегистрировали, добавим функцию отката в список
+                    rollback_list.push(rollback);
+                }
+                Err(e) => {
+                    // если произошла ошибка, проходимся по списку и откатываем все регистрации.
+                    for rollback in rollback_list {
+                        if let Err(e) = rollback() {
+                            error!("откат транзакции: {e}")
+                        }
+                    }
+                    return Err(e.to_string());
+                }
+            };
+        }
+
+        Ok(())
+    }
     pub fn run_shortcut(&self) {}
 }
